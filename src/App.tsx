f4fb9609de30c50405c039d2e7f91839b7e14ac3
jwt2706 <jwt2home@gmail.com>
2024-12-24 15:43:37 -0500

    fix page link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useEffect, FC } from "react";
-import { useRoutes, useNavigate, BrowserRouter as Router, Navigate } from "react-router-dom";
+import { FC } from "react";
+import { useRoutes, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 
 const Navigation: FC = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/");
-  }, [navigate]);
-
   const routes = useRoutes([
     { path: "/old", element: <Home /> },
   ]);
